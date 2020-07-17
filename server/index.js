17382c25df8a6be54afc0edf93282f810c2b3b91
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-17 13:51:22 -0700

    proxy implimented for my component

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const httpProxy = require('http-proxy');
 const path = require('path');
 const app = express();
 const port = 3000;
+const apiProxy = httpProxy.createProxyServer({ prependPath: false });
 
 // app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
+
+
+app.get("/carousels/:id", function(req, res) {
+  let { id } = req.params;
+  console.log('preview endpoint hit with id: ', id);
+  apiProxy.web(req, res, {target: `http://localhost:3003/${id}`});
+});
+
+apiProxy.on('error', (err) => {
+  console.log('proxy error: ', err);
+});
+
